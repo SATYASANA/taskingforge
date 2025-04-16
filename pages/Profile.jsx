34479a4307884ca55f60c7c3e9f3a10a106b428d
SATYASANA <satyabratasana4@gmail.com>
2025-04-17 01:07:42 +0530

    profile page updates

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { getProfile } from '../Redux/Slices/AuthSlice'
 
 export default function Profile() {
-    const dispatch = useDispatch()
-    const res = dispatch(getProfile())
-    console.log(res)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getProfile())
+      .unwrap()
+      .then((data) => {
+        console.log("Profile data:", data)
+      })
+      .catch((err) => {
+        console.error("Error fetching profile:", err)
+      })
+  }, [dispatch])
+
   return (
     <div>
-       {/* {res} */}
+      <h1>Profile Page</h1>
     </div>
   )
 }
