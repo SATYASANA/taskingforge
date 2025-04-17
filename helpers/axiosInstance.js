327d4861c2f7e1c41dd6d77ec2a6d6fdbcafca56
SATYASANA <satyabratasana4@gmail.com>
2025-04-17 20:54:47 +0530

    change the api url

--- helpers/axiosInstance.js
+++ helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-// const BASE_URL = 'https://taskforge-production-2ddb.up.railway.app/api/v1'
-const BASE_URL = 'http://localhost:10000/api/v1'
+const BASE_URL = 'https://taskforge-production-2ddb.up.railway.app/api/v1'
+// const BASE_URL = 'http://localhost:10000/api/v1'
 
 const axiosInstance = axios.create()
 axiosInstance.defaults.baseURL = BASE_URL
