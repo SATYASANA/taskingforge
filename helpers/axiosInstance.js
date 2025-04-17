f85dc0bd1e84f9424014aa2f91a85225e3ad23ea
SATYASANA <satyabratasana4@gmail.com>
2025-04-17 23:07:50 +0530

    hiiii

--- helpers/axiosInstance.js
+++ helpers/axiosInstance.js
@@ -1,10 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = 'https://taskforge-production-2ddb.up.railway.app/api/v1'
-// const BASE_URL = 'http://localhost:10000/api/v1'
+const BASE_URL = 'https://taskforge-production-2ddb.up.railway.app/api/v1';
+// const BASE_URL = 'http://localhost:10000/api/v1';
 
-const axiosInstance = axios.create()
-axiosInstance.defaults.baseURL = BASE_URL
-axiosInstance.defaults.withCredentials = true
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // 👈 this ensures cookies are sent in cross-origin requests
+});
 
-export default axiosInstance
+export default axiosInstance;
